8c6ed48bd2405af3d1ba0e65bdc652fcf130f2f1
Matheus Enes Santos Viana <matheus.viana@verycreatives.com>
2019-11-24 02:18:05 -0300

    Remove uncessary Delete button repeater initalizer as param

--- duplicate-form-isiaformrepeater/demo/js/demo.js
+++ duplicate-form-isiaformrepeater/demo/js/demo.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 
   $('#base').isiaFormRepeater({
     addButton: '<div class="repeat-add-wrapper"><a data-repeat-add-btn class="repeat-add pure-button pure-button-primary" href="#">Add</a></div>',
-    removeButton: '<a data-repeat-remove-btn class="repeat-remove pure-button pure-button-primary" href="#">Remove</a>',
     initNewGroup: (group) => initRepeater(group),
   });
 
